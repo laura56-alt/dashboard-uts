36ac7f6b04dc687dbb3f8d90156e843c43b2bdca
laura56-alt <laura56@mhs.usk.ac.id>
2025-11-11 22:22:15 +0700

    Refactor app.py to include model loading and imports

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+import streamlit as st
+import requests
+import tempfile
+from PIL import Image
+import tensorflow as tf   # ⬅️ Tambahkan baris ini
+import torch
+from torchvision import transforms
+import cv2
+import numpy as np
 import tempfile, requests
 
 # === Muat Model Klasifikasi ===
@@ -18,4 +27,5 @@ tmp_pt.write(r.content)
 tmp_pt.flush()
 deteksi_model = torch.load(tmp_pt.name, map_location=torch.device('cpu'))
 deteksi_model.eval()
+
 
